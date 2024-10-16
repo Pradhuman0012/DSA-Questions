e60f617d201c9d04b473b16eb3ce84a7c3acc12d
pradhuman tamboli <pradhumantamboli@pradhumans-MacBook-Air.local>
2024-10-16 12:43:17 +0530

    Rename files

--- 01_bubble_sort.py
+++ 01_bubble_sort.py
@@ -1,20 +1,17 @@
 # sorts a list of numbers in ascending order without using any built-in sorting functions.
+"""
+Sorts an array using the bubble sort algorithm.
+
+Input: [64, 34, 25, 12, 22, 11, 90]
+Output: [11, 12, 22, 25, 34, 64, 90]
+"""
 
 def bubble_sort(nums):
-    """
-    Sorts an array using the bubble sort algorithm.
-    
-    Parameters:
-    nums (list): A list of numbers to be sorted.
-    
-    Returns:
-    list: The sorted list.
-    """
     for i in range(len(nums)):
         for j in range(i+1,len(nums)):
             if nums[i]>nums[j]:
                 nums[i],nums[j]=nums[j],nums[i]
-    print(nums)
+    return nums
 
 
 nums = [64, 34, 25, 12, 22, 11, 90]
