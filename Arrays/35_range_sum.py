e6cbfb4273292b71bf552855da71778bce507345
pradhuman tamboli <pradhumantamboli12@gmail.com>
2024-12-14 23:46:16 +0530

    update naming convention for better understanding

--- Arrays/35_range_sum.py
+++ Arrays/35_range_sum.py
@@ -23,13 +23,13 @@ def RangeSumPrefix(a,b):
     prefix[0]=a[0]
     n=len(a)
     res=[]
-    temptotal=0
+    total=0
     for i in range(1,n):
         prefix[i]=prefix[i-1]+a[i]
 
-    for i in b:
-        temptotal=prefix[i[1]] if i[0]<=0 else prefix[i[1]]-prefix[i[0]-1]
-        res.append(temptotal)
+    for start, end in b:
+        total=prefix[end]-(prefix[start-1] if start>0 else 0)
+        res.append(total)
     return res
 
 a = [1,2,3,4,5]
@@ -38,3 +38,4 @@ b = [[0,3],[1,2]]
 
 res=RangeSumPrefix(a,b)
 print(res)
+# time complexity O(n)
