b792bbb323578901469ac1b1f8b81d89c61f3c2d
pradhuman tamboli <pradhumantamboli12@gmail.com>
2024-12-15 15:06:01 +0530

    add brute force solution of count pairs problem

--- Arrays/38_count_pairs.py
+++ Arrays/38_count_pairs.py
@@ -3,8 +3,8 @@ input_string="baagdcag"
 # op=5 
 count=0
 for i in range(len(input_string)):
-    for j in range(i+1,len(input_string)):
-        if input_string[i]==pair[0]:
+    if input_string[i]==pair[0]:
+        for j in range(i+1,len(input_string)):
             if input_string[j]==pair[1]:
                 count+=1
 print(count)
