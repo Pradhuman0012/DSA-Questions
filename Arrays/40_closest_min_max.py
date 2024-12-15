759a2c74038fcab295214384b46bd04d8b385662
pradhuman tamboli <pradhumantamboli12@gmail.com>
2024-12-15 22:52:03 +0530

    Find closest min-max subarray in array - opimal approch

--- Arrays/40_closest_min_max.py
+++ Arrays/40_closest_min_max.py
@@ -8,6 +8,29 @@ Ex -[ 1 2 3 1 3 4 6 4 6 3 ]
 ans [3-6] => [1 3 4 6] length = 4
 
 '''
+# TC = O(N^2)
+# SC = O(1)
+
+# def closest_min_max(arr):
+#     n=len(arr)
+#     mx=arr[0]
+#     mn=arr[0]
+#     for num in arr:
+#         mx= num if num>mx else mx
+#         mn= num if num<mn else mn
+#     res=n
+
+#     for i in range(n):
+#         if arr[i]==mn:
+#             for j in range(n):
+#                 if arr[j]==mx:
+#                     l= (j-i)+1 if j>i else (i-j)+1
+#                     if l<res:
+#                         res=l
+#     return res
+
+# -------------------------------
+# opptimal -
 # TC = O(N)
 # SC = O(1)
 
@@ -15,19 +38,30 @@ def closest_min_max(arr):
     n=len(arr)
     mx=arr[0]
     mn=arr[0]
+    min_index=-1
+    max_index=-1
+
     for num in arr:
         mx= num if num>mx else mx
         mn= num if num<mn else mn
-    res=n
+    ans=n
 
-    for i in range(n):
+
+    for i in range(n-1,-1,-1):
         if arr[i]==mn:
-            for j in range(n):
-                if arr[j]==mx:
-                    l= (j-i)+1 if j>i else (i-j)+1
-                    if l<res:
-                        res=l
-    return res
+            min_index=i
+            if max_index != -1:
+                res=(max_index-min_index)+1
+                if res<ans:
+                    ans=res
+
+        if arr[i]==mx:
+            max_index=i
+            if min_index != -1:
+                res=(min_index-max_index)+1
+                if res<ans:
+                    ans=res
+    return ans
 
 arr=[1,2,3,1,3,4,6,4,6,3]
 # arr=[2,2,6,4,5,1,5,2,6,4,1]
